5567c1573be8c5e95e288f85ffdceeef91b0dfae
Sung-Jong Will Kim <willicious.k@gmail.com>
2018-05-24 13:15:37 +0900

    [working] on doubly linked list
    - studying doubly linked list
    
    - none

--- 0.dataStructures/LinkedList.swift
+++ 0.dataStructures/LinkedList.swift
@@ -17,10 +17,7 @@ public class Node<Value> {
 extension Node: CustomStringConvertible {
 
 	public var description: String {
-		guard let next = next else {
-			return "\(value)"
-		}
-		return "\(value) -> " + String(describing: next) + " "
+		return "\(value)"
 	}
 
 }
@@ -37,15 +34,71 @@ public struct LinkedList<Value> {
 		return head == nil
 	}
 
+	// insert at front
+	public mutating func push(_ value: Value) {
+		head = Node(value: value, next: head, previous: nil)
+		count += 1
+		if tail == nil {
+			tail = head
+		}
+	}
+
+	public mutating func append(_ value: Value) {
+		guard !isEmpty else {
+			push(value)
+			return
+		}
+		count += 1
+		tail!.next = Node(value: value, next: nil, previous: tail)
+		tail = tail!.next
+	}
+
+	// getter
+	public func node(at index: Int) -> Node<Value>? {
+		guard index >= 0, count > 0, count > index else {
+			return nil
+		}
+
+		if index == 0 {
+			return head
+		}
+
+		var currentNode = head
+		for _ in 1...index {
+			currentNode = currentNode!.next
+		}
+		return currentNode
+	}
+
+	@discardableResult
+	public mutating func insert(_ value: Value, after node: Node<Value>) -> Node<Value> {
+		guard tail !== node else {
+			append(value)
+			return tail!
+		}
+		let target = Node(value: value, next: node.next, previous: node)
+		node.next!.previous = target
+		node.next = target
+		count += 1
+		return target
+	}
+	
 }
 
 extension LinkedList: CustomStringConvertible {
 
 	public var description: String {
-		guard let head = head else {
+		guard let head = head else { // count > 0
 			return "It's Empty"
 		}
-		return String(describing: head)
+
+		var currentNode = head
+		var result: String = String(describing: currentNode)
+		for _ in 1..<count {
+			currentNode = currentNode.next!
+			result +=  " <=> " + String(describing: currentNode)
+		}
+		return result
 	}
 
 }
--- dsTest/main.swift
+++ dsTest/main.swift
@@ -1,3 +1,21 @@
 import Foundation
 
 var list = LinkedList<Int>()
+
+list.push(1)
+list.push(2)
+list.push(3)
+list.append(1)
+list.append(2)
+list.append(3)
+
+print(list)
+print("list.count: \(list.count)")
+print("+======+")
+let target = list.node(at: 2)!
+print("node at 2: \(target)")
+print("+======+")
+for _ in 1...4 {
+  list.insert(-1, after: target)
+}
+print(list)
