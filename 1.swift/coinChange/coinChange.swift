4af62cf62c460eea82f5e22631fb4321b534a7d0
Sung-Jong Will Kim <willicious.k@gmail.com>
2018-05-28 14:11:19 +0900

    [solved] coin change problem
    - solved coin change problem using dynamic programming
    
    - none

--- 1.swift/coinChange/coinChange.swift
+++ 1.swift/coinChange/coinChange.swift
@@ -1,10 +1,42 @@
 import Foundation
 
 // Helpers, if needed
+func printMatrix(target: [[Int]]) {
+  let width = target[0].count
+  let height = target.count
+
+  var lane = ""
+  for i in 0..<height {
+    lane = ""
+    for j in 0..<width {
+      lane += String(target[i][j]) + " "
+    }
+    print(lane)
+  }
+}
 
 // Logic
 func solveCase(n: Int, c:[Int]) -> Int {
-  return 0
+  var cache: [[Int]] = []
+
+  for i in 0...c.count {
+    let lane: [Int] = Array<Int>(repeating: 0, count: n+1)
+    cache.append(lane)
+    cache[i][0] = 1
+  }
+
+  for i in 1...c.count {
+    for j in 1...n {
+      if (c[i-1] <= j) {
+				cache[i][j] = cache[i-1][j] + cache[i][j-c[i-1]]
+			} else {
+				cache[i][j] = cache[i-1][j]
+			}
+    }
+  }
+
+  printMatrix(target: cache)
+  return cache[c.count][n]
 }
 
 // Main: Input Handling
@@ -13,7 +45,7 @@ let cases = Int(readLine()!)!
 for _ in 1...cases {
   let firstLine = readLine()!.components(separatedBy: " ").map { Int($0)! }
   let secondLine = readLine()!.components(separatedBy: " ").map { Int($0)! }
-  
+
   let answer = solveCase(n: firstLine[0], c: secondLine)
-  print(answer)
+  print("answer: \(answer)")
 }
