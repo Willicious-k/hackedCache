70e6802cf7fcaa3eed288fc7e22535a8e3f1e66c
Sung-Jong Will Kim <willicious.k@gmail.com>
2018-06-02 14:56:22 +0900

    [added] fairRation template
    - added answer template
    
    - none

--- 1.swift/fairRation/fairRation.swift
+++ 1.swift/fairRation/fairRation.swift
@@ -3,5 +3,16 @@ import Foundation
 // Helpers
 
 // Logic
+func fairRations(B: [Int]) -> Int {
+  return 0
+}
 
 // main - input handling
+let cases = Int(readLine()!)!
+
+for _ in 1...cases {
+  let _ = Int(readLine()!)!
+  let array = readLine()!.components(separatedBy: " ").map{ Int($0)! }
+  let result = fairRations(B: array)
+  print(result)
+}
